95177ebfc2248d413b285863784172e7b326303b
arthuramorim <arthurpas67@gmail.com>
2022-02-04 14:23:12 -0300

    init project

---
+++ src/components/book/book.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Book(props) {
+    const { title, description, iconColor, vertical } = props;
+    return (
+        <>
+        <h2>Book</h2>
+        </>
+    );
+}
+
